93c86713f33b961fd6bd8acfbe1285f51c9a96ea
Emanuelle Coelho <evicacoelho@gmail.com>
2026-02-11 14:52:14 -0300

    hotfix at react routing

--- frontend/server.js
+++ frontend/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Handle React routing, return all requests to React app
-app.get('*', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
